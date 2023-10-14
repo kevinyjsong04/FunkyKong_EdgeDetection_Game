0b639b9351ce42afbf8a0c9ca41f5990fce37db5
--add <kevinyjsong04@gmail.com>
2023-10-13 23:49:31 -0400

    added initial project

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import P5Wrapper from 'react-p5-wrapper';
+// import sketch from './sketches/sketch';
+import './App.css';
+
+function App() {
+    // return <P5Wrapper sketch={sketch} color={this.state.color}></P5Wrapper>
+    return <P5Wrapper sketch={sketch} color={this.state.color}></P5Wrapper>
+}
+
+export default App
